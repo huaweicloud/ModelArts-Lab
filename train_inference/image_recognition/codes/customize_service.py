3826f2f2d288bcf6665751cf82a32eb73e5d5579
chaoxu1203 <chaoxu1203@users.noreply.github.com>
2019-07-05 16:47:33 +0800

    update image recognition train and infer case (#252)
    * Delete image_recongition.ipynb
    
    * Add files via upload
    
    * Delete dog_and_cat_train.py
    
    * Delete customize_service.py
    
    * Add files via upload

--- train_inference/image_recognition/codes/customize_service.py
+++ train_inference/image_recognition/codes/customize_service.py
@@ -11,14 +11,14 @@ class dogcat_service(TfServingBaseService):
         image = Image.open(file_content)
         image = image.convert('RGB')
         image = np.asarray(image, dtype=np.float32)
-        image = cv2.resize(image, (64, 64), interpolation=cv2.INTER_CUBIC)
+        image = cv2.resize(image, (128, 128), interpolation=cv2.INTER_CUBIC)
         image = image[np.newaxis, :, :, :]
         preprocessed_data[k] = image
     return preprocessed_data
 
   def _postprocess(self, data):
     outputs = {}
-    logits = data['logits'][0][0]
+    logits = data['logits'][0][1]
     label = "dog" if logits > 0.5 else "cat"
     outputs['predict label'] = label
     confidence = logits if logits > 0.5 else 1 - logits
