81bc33b63c8fedc1b4c70f45cc500759187d784c
pansgg <4226248@qq.com>
2026-08-06 16:45:21 +0800

    install mooncake openssl etcd

--- install_all.sh
+++ install_all.sh
@@ -144,3 +144,138 @@ pip install "ray>=2.47.1,<=2.48.0" "protobuf>3.20.0" "urllib3==1.26.11"
 
 cd "$current_path"/ascend_vllm/scripts/
 bash patch_third_pkg.sh
+
+# install mooncake
+if [ "$compile_mooncake" = "TRUE" ]; then
+  echo "[install_all] Installing mooncake with source code (compile_mooncake=${compile_mooncake})"
+  cd "$current_path"
+  openeuler_yum_mirror="[openEuler-everything]
+name=openEuler-everything
+baseurl=http://mirrors.huaweicloud.com/openeuler/openEuler-22.03-LTS-SP4/everything/aarch64/
+enabled=1
+gpgcheck=0
+gpgkey=http://mirrors.huaweicloud.com/openeuler/openEuler-22.03-LTS-SP4/everything/aarch64/RPM-GPG-KEY-openEuler
+
+[openEuler-EPOL]
+name=openEuler-epol
+baseurl=http://mirrors.huaweicloud.com/openeuler/openEuler-22.03-LTS-SP4/EPOL/main/aarch64/
+enabled=1
+gpgcheck=0
+
+[openEuler-update]
+name=openEuler-update
+baseurl=http://mirrors.huaweicloud.com/openeuler/openEuler-22.03-LTS-SP4/update/aarch64/
+enabled=1
+gpgcheck=0"
+  hce_yum_mirror="[base]
+name=HCE 3.0 base
+baseurl=https://repo.huaweicloud.com/hce/3.0/os/aarch64/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.huaweicloud.com/hce/3.0/os/RPM-GPG-KEY-HCE-3
+
+[updates]
+name=HCE 3.0 updates
+baseurl=https://repo.huaweicloud.com/hce/3.0/updates/aarch64/
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.huaweicloud.com/hce/3.0/updates/RPM-GPG-KEY-HCE-3"
+  rm -rf /etc/yum.repos.d/*.repo
+  echo -e "${openeuler_yum_mirror}" | tee /etc/yum.repos.d/EulerOS.repo > /dev/null
+  echo -e "${hce_yum_mirror}" | tee /etc/yum.repos.d/HCE.repo > /dev/null
+  cat /etc/yum.repos.d/EulerOS.repo
+  cat /etc/yum.repos.d/HCE.repo
+  yum install -y rdma-core-devel \
+                 gflags-devel \
+                 yaml-cpp-devel \
+                 gtest-devel \
+                 jsoncpp-devel \
+                 libunwind-devel \
+                 numactl-devel \
+                 boost-devel \
+                 boost-system \
+                 boost-thread \
+                 openssl-devel \
+                 grpc-devel \
+                 protobuf-devel \
+                 protobuf-compiler \
+                 libcurl-devel \
+                 hiredis-devel \
+                 patchelf \
+                 libzstd-devel \
+                 xxhash-devel \
+                 libcurl-devel \
+                 glog-devel \
+                 etcd
+
+  git clone -b v0.3.11.post1 https://github.com/kvcache-ai/Mooncake.git
+  cd "$current_path"/Mooncake/
+
+  # Check if .gitmodules exists
+  if [ -f ".gitmodules" ]; then
+    echo "Initializing git submodules..."
+    git submodule sync --recursive
+    git submodule update --init --recursive
+    echo "Git submodules initialized and updated successfully"
+  else
+    echo -e "No .gitmodules file found. Skipping..."
+    exit 1
+  fi
+
+  cd ./extern/yalantinglibs
+  mkdir build
+  cd build
+  cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARK=OFF -DBUILD_UNIT_TESTS=OFF
+  cmake --build . -j$(nproc)
+  cmake --install .
+
+  cd "$current_path"/Mooncake/extern/
+  go_version=1.25.9
+  if command -v go &> /dev/null && [ "$(go version | awk '{print $3}')" == "${go_version}" ]; then
+    echo "Go ${go_version} installed. Skipping..."
+  else
+    arch=$(uname -m)
+    if [ "${arch}" == "aarch64" ] || [ "${arch}" == "x86_64" ]; then
+      arch="arm64"
+    else
+      echo "Unsupported architecture: ${arch}"
+      exit 1
+    fi
+    wget -q --show-progress http://mirrors.aliyun.com/golang/go${go_version}.linux-${arch}.tar.gz
+    tar -zxf go${go_version}.linux-${arch}.tar.gz -C /usr/local/
+    rm -rf go${go_version}.linux-${arch}.tar.gz
+  fi
+  if ! grep -q "export PATH=\$PATH:/usr/local/go/bin" ~/.bashrc; then
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+  fi
+  export PATH=$PATH:/usr/local/go/bin
+  go env -w GO111MODULE=on
+  go env -w GOPROXY=http://mirrors.huaweicloud.com/repository/goproxy/
+  go env -w GONOSUMDB=*
+
+  cd "$current_path"/Mooncake/extern/
+  git clone -b cpp-7.0.0 https://github.com/msgpack/msgpack-c.git
+  cp -r msgpack-c/include/* /usr/local/include/
+  export CPLUS_INCLUDE_PATH=/usr/local/include:${CPLUS_INCLUDE_PATH}
+
+  cd "$current_path"/Mooncake/
+  rm -rf build
+  mkdir build
+  cd build
+  cmake -DUSE_ASCEND_DIRECT=ON -DUSE_CUDA=OFF -DCMAKE_POLICY_VERSION_MINIMUM=4.0 -DUSE_ETCD=ON -DSTORE_USE_ETCD=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_EXAMPLES=OFF -DWITH_STORE_RUST=OFF ..
+  make -j
+  make install
+  ldconfig
+else
+  echo "[install_all] Installing mooncake with wheel package"
+  pip install mooncake_transfer_engine_npu==0.3.11.post1
+fi
+
+# rebuild openssl
+wget --no-check-certificate https://github.com/openssl/openssl/releases/download/openssl-3.4.5/openssl-3.4.5.tar.gz
+tar -zxf openssl-3.4.5.tar.gz
+cd openssl-3.4.5
+mkdir -p /home/ma-user/AscendCloud/openssl
+./config enable-md2 --prefix=/home/ma-user/AscendCloud/openssl --openssldir=/home/ma-user/AscendCloud/openssl
+make -sj && make install -sj
+echo 'export LD_PRELOAD=/home/ma-user/AscendCloud/openssl/lib/libcrypto.so.3:/home/ma-user/AscendCloud/openssl/lib/libssl.so.3' >> ~/.bashrc
