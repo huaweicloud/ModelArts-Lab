c02ab7755f3b92817956ac3833f5c5a93fceacfc
chn-phc <phcking0219@163.com>
2026-09-12 17:01:12 +0800

    fix SWA recycling by decrementing _inflight in update_from_output (#1912)
    * fix SWA recycling by decrementing _inflight in update_from_output
    
    * fix SWA recycling by decrementing _inflight in update_from_output

--- ascend_vllm/patch/platform/patch_recompute_scheduler.py
+++ ascend_vllm/patch/platform/patch_recompute_scheduler.py
@@ -817,6 +817,15 @@ def _patch_update_from_output(
     scheduler_output: SchedulerOutput,
     model_runner_output: ModelRunnerOutput,
 ) -> dict[int, EngineCoreOutputs]:
+    # adapt begin : fix SWA recycling by decrementing _inflight in update_from_output:https://github.com/vllm-project/vllm-ascend/pull/13047
+    from vllm_ascend.patch.platform.patch_swa_inflight_free import _inflight
+    for rid, n in scheduler_output.num_scheduled_tokens.items():
+        v = _inflight.get(rid, 0) - n
+        if v <= 0:
+            _inflight.pop(rid, None)
+        else:
+            _inflight[rid] = v
+    # adapt end
     sampled_token_ids = model_runner_output.sampled_token_ids
     logprobs = model_runner_output.logprobs
     prompt_logprobs_dict = model_runner_output.prompt_logprobs_dict
